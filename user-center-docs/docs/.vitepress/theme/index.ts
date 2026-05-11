b7b44345adcc31b4bcc928954c11e1edd86b652a
LinCanhui <lingyun2311@gmail.com>
2026-05-11 18:36:31 +0800

    refactor: 从仓库中移除所有源代码和项目文档

--- user-center-docs/docs/.vitepress/theme/index.ts
+++
@@ -1,7 +0,0 @@
-import BaseTheme from "vitepress/theme";
-import type {Theme} from "vitepress";
-import "./style/vars.css";
-
-export default {
-    extends: BaseTheme,
-} satisfies Theme;
--- user-center-vue-frontend/openapi2ts.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    requestLibPath: "import request from '@/libs/request';",
-    schemaPath: "http://localhost:8100/api/v3/api-docs",
-    authorization: "Basic " + Buffer.from("admin:admin123").toString("base64"),
-    serversPath: "./src",
-};
